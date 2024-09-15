0c01fa57b248812fe3dbaee16fde16e55f14c38c
Borneto <bornaflinta2005@gmail.com>
2024-09-15 14:07:40 +0200

    ConfigContext with zod

--- contexts/ConfigContext.js
+++ contexts/ConfigContext.js
@@ -1,74 +1,71 @@
 const config = require('../config.json');
+const { z } = require('zod')
+
+const ConfigContextScheme = z.object({
+    appName: z.string(),
+    language: z.string(),
+    blacklistEmail: z.string().email(),
+
+    smtpHost: z.string(),
+    smtpPort: z.number(),
+    smtpUsername: z.string(),
+    smtpPassword: z.string(),
+  
+    smtpFromName: z.string(),
+    smtpFromAddress: z.string().nullable(),
+  
+    imapHost: z.string(),
+    imapPort: z.number(),
+    imapUsername: z.string().email(),
+    imapPassword: z.string(),
+  
+    aliasHelp: z.string().email(),
+    aliasUnlock: z.string().email(),
+    aliasBlacklist: z.string().email(),
+    aliasUnblacklist: z.string().email(),
+  
+    supportEmail: z.string().email(),
+  
+    databaseHost: z.string(),
+    databasePort: z.number(),
+    databaseName: z.string(),
+    databaseUsername: z.string(),
+    databasePassword: z.string(),
+  
+    admins: z.array(z.string()),
+  
+    discordBotToken: z.string(),
+    discrodClientId: z.string()
+})
+
+/** @typedef { z.infer<typeof ConfigContextScheme>} Config */
 
 class ConfigContext {
     static instance = null;
 
     /**
-     * Return ConfigContext global instance
-     * @returns {ConfigContext}
+     * @returns {Config}
      */
     static getInstance() {
         if (ConfigContext.instance)
-            return ConfigContext.instance;
+            return ConfigContext.instance.config;
 
         ConfigContext.instance = new ConfigContext();
-        return ConfigContext.instance;
+        return ConfigContext.instance.config;
     }
 
     constructor() {
-        if (!config)
-            console.warn('Failed to require config file');
-
-        const cfg = config || {};
-
-        this.appName = cfg.appName || 'FERify';
-        this.language = cfg.language || 'en';
-        this.blacklistEmail = cfg.blacklistEmail || 'blacklist.ferify@example.com';
-
-        if (!cfg.smtpHost || !cfg.smtpPort || !cfg.smtpUsername || !cfg.smtpPassword)
-            console.warn('Incomplete SMTP configuration provided');
-        
-        this.smtpHost = cfg.smtpHost || 'smtp.example.com';
-        this.smtpPort = cfg.smtpPost || 587;
-        this.smtpUsername = cfg.smtpUsername || 'ferify@example.com';
-        this.smtpPassword = cfg.smtpPassword || 'password';
-        
-        this.smtpFromName = cfg.smtpFromName || 'FERify';
-        this.smtpFromAddress = cfg.smtpFromAddress || this.smtpUsername;
-
-        if (!cfg.imapHost || !cfg.imapPort || !cfg.imapUsername || !cfg.imapPassword)
-            console.warn('Incomplete IMAP configuration provided');
-
-        this.imapHost = cfg.imapHost || 'imap.example.com';
-        this.imapPort = cfg.imapPort || 993;
-        this.imapUsername = cfg.imapUsername || 'ferify@example.com';
-        this.imapPassword = cfg.imapPassword || 'password';
-    
-        this.aliasHelp = cfg.aliasHelp || 'ferify@example.com';
-        this.aliasUnlock = cfg.aliasUnlock || 'unlock.ferify@example.com';
-        this.aliasBlacklist = cfg.aliasBlacklist || 'blacklist.ferify@example.com';
-        this.aliasUnblacklist = cfg.aliasUnblacklist || 'unblacklist.ferify@example.com';
-
-        this.supportEmail = cfg.supportEmail || 'support@example.com';
-
-        if (!cfg.databaseHost || !cfg.databasePort || !cfg.databaseName || !cfg.databaseUsername || !cfg.databasePassword)
-            throw Error("Incomplete database configuration provided");
-
-        this.databaseHost = cfg.databaseHost;
-        this.databasePort = cfg.databasePort;
-        this.databaseName = cfg.databaseName;
-        this.databaseUsername = cfg.databaseUsername;
-        this.databasePassword = cfg.databasePassword;
-
-        this.admins = Array.isArray(cfg.admins) ? 
-            cfg.admins.filter(admin => typeof(admin) == 'string') : [];
-
-        if (!cfg.discordBotToken || !cfg.discrodClientId)
-            console.warn('Incomplete discord bot configuration provided');
-
-        this.discordBotToken = cfg.discordBotToken || 'INVALID_TOKEN';
-        this.discordClientId = cfg.discrodClientId || 'INVALID_CLIEND_ID'
+        try{
+            this.config = ConfigContextScheme.parse(config)
+        }
+        catch(err){
+            if(err instanceof z.ZodError){
+                const paths = err.errors.map(error => error.path);
+                console.error('Config is missing:', paths);
+            }
+            else
+                console.log(err)
+        }
     }
 }
-
 exports.ConfigContext = ConfigContext;
